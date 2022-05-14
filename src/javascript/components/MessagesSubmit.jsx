e3a2b2cd1f6c6a455b6dc19bba9c5d7cd84af1a5
FloreaLaura <florealaura18@stud.ase.ro>
2022-05-14 14:05:09 +0300

    new

--- src/javascript/components/MessagesSubmit.jsx
+++ src/javascript/components/MessagesSubmit.jsx
@@ -18,7 +18,7 @@ function MessagesSubmit() {
                 });
 
                 if(response.status === 200) {
-                    alert(`\nMessage sent: ${response.data.translationData.translatedText}`);
+                    alert(`\nTranslated Text: ${response.data.translationData.translatedText}`);
                         
                 }
         }
